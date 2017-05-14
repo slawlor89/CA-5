86422d0e4423d7e965fbe43d86a440701a7ff50b
Lawlor <steven.d.lawlor@accenture.com>
2017-05-14 16:51:14 +0100

    Commit 1: First commit

---
+++ myCalculator.R
@@ -0,0 +1,9 @@
+# Student Number 10354686
+# Student Name: Steven Lawlor
+# Continuous Assignment 5
+
+# The following R script is used to create a set of 10 functions as part of a 
+# calculator. The calculator will allow the user the ability to perform the following
+# functions;
+# XXXXXX
+
